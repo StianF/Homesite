9916c545eb7769898cf1ef18a47d23dda358a597
Stian Fredrikstad <stian@fredrikstad.de>
2011-09-18 20:13:09 +0200

    capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :rvm_ruby_string, '1.9.2-p290'
 
 set :default_environment, {
   'PATH' => "/usr/local/rvm/gems/ruby-1.9.2-p290/bin:/usr/local/rvm/rubies/ruby-1.9.2-p290/bin:/usr/local/rvm/bin/:$PATH",
-#  'RUBY_VERSION' => '1.9.2p290',
+  'RUBY_VERSION' => 'ruby 1.9.2',
   'GEM_HOME'     => '/usr/local/rvm/gems/ruby-1.9.2-p290',
   'GEM_PATH'     => '/usr/local/rvm/gems/ruby-1.9.2-p290',
   'BUNDLE_PATH'  => '/usr/local/rvm/gems/ruby-1.9.2-p290'  # If you are using bundler.
